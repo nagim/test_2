6de1353c216dc349d58905e5a4f504774f828af8
Imre Nagy <imre.nagy@ericsson.com>
2016-04-13 15:50:23 +0200

    Update styles/test.js

--- styles/test.js
+++ styles/test.js
@@ -1,4 +1,4 @@
-jQuery(window).bind('onbeforeunload' ,function(){
+gitbook.events.bind("page.change", function(){
 $(document).ready(function() {
 $("pre").contents()
         .filter(function(){return this.nodeType === 3}).add(".red")
