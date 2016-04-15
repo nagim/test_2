c57fbb1714639a2d9dd1346998ed72a3a5610756
Imre Nagy <imre.nagy@ericsson.com>
2016-04-15 08:22:39 +0200

    Update styles/test.js

--- styles/test.js
+++ styles/test.js
@@ -1,11 +1,9 @@
 $(window).load(function() {
-$(document).ready(function() {
 $("pre").one("load", function () {
 var b = $("button")
 b.remove()
 $(this).wrapInner('<code />')
 $(this).append($(b))
 runPage()
-});
 });
 });
