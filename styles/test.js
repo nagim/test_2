59a4366d97e6dd52037f2ba8a8c6c3900b823923
Imre Nagy <imre.nagy@ericsson.com>
2016-04-15 08:47:25 +0200

    Update styles/test.js

--- styles/test.js
+++ styles/test.js
@@ -1,9 +1,13 @@
+window.onload=function(){
+$(document).ready(function() {
 $("pre").one("load", function () {
 $(this).wrapInner('<code />')
-//var b = $("button")
-//b.remove()
-//$($("code")).after($("button"))
-//$(this).append($(b))
+var b = $("button")
+b.remove()
+$($("code")).after($("button"))
+$(this).append($(b))
 //$("pre").contents().filter(function(){return this.nodeType === 3}).wrap('<code />')
 //$("code").add($(".red").wrap('<code />'))
 });
+});
+}
